5201501a1473105784d6b03dfb50a241fb4bdbb4
Luthfi Hadiwidjaja <Luthfihadi78@gmail.com>
2026-05-06 14:48:34 +0700

    Belajar array javascript

--- 00-starting-js-proj/assets/scripts/app.js
+++ 00-starting-js-proj/assets/scripts/app.js
@@ -1,6 +1,49 @@
 //import { apiKey } from "./util.js";
 //import apiKey from "./util.js"
 
-import * as Util from "./util.s";
+import * as Util from "./util.js";
 console.log(Util.name);
 console.log("Hello world")
+
+
+function greetUser(userName, message = "Budsss"){
+    // console.log(userName);
+    // console.log(message);'
+    return "Hi my name " + userName + " " + message;
+}
+
+
+const greetUser1 = 
+function get(){
+    console.log("Hello Qkuk");
+}
+
+const user = {
+    name : "Qkuk",
+    age : 30
+};
+
+
+class User{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const hobbies = ["Gaming","Reading", "Dancing"];
+
+hobbies.push("Working")
+console.log(hobbies);
+
+const index = hobbies.findIndex((item) => item === "Sports");
+console.log(index);
+
+const editedHobbies = hobbies.map((item) => item + "!");
+console.log(editedHobbies);
+
+const user1 = new User("Budi", 34);
+console.log(user1);
+
+console.log(user);
+console.log(greetUser("Qkuk"))
