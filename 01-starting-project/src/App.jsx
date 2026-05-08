b4dc46e8d37b8e88a369cb7e05ef203332c180b6
Luthfi Hadiwidjaja <Luthfihadi78@gmail.com>
2026-05-08 14:51:17 +0700

    Props dynamic

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,4 +1,6 @@
 import reactImg from './assets/react-core-concepts.png';
+import imageReact from './assets/components.png'
+import { CORE_CONCEPTS } from './data';
 
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
@@ -17,15 +19,42 @@ const description = reactDescriptions[getRandomInt(2)];
         {description} Fundamental React concepts you will need for almost any app you are
         going to build!
       </p>
+
     </header>
   );
+}
+
+function CoreConcepts({image, title, description}){
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  )
 }
 
 function App() {
   return (
     <div>
-      <Header></Header>
+      <Header/>
       <main>
+        <section id='core-concepts'>
+          <h2>Core Concepts</h2>
+          <ul>
+              <CoreConcepts
+                title={CORE_CONCEPTS[0].title}
+                description={CORE_CONCEPTS[0].description}
+                image = {CORE_CONCEPTS[0].image}
+                />
+              <CoreConcepts {...CORE_CONCEPTS[1]}/>  
+              <CoreConcepts {...CORE_CONCEPTS[2]}/>  
+              <CoreConcepts {...CORE_CONCEPTS[3]}/>  
+              
+
+
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
