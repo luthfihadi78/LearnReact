cbce2b5bc347be8a522901b6dc36b02a1f1f83d7
Moch. Luthfi Hadiwidjaja <Luthfihadi78@gmail.com>
2026-05-20 14:41:10 +0700

    add tabContent

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -8,7 +8,7 @@ import { useState } from 'react';
 import { EXAMPLES } from './data.js'
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('components')
+  const [selectedTopic, setSelectedTopic] = useState()
 
   function handleSelect(selectedButton){
   setSelectedTopic(selectedButton)
@@ -16,9 +16,24 @@ function App() {
   }
   console.log('App Components Executinng')
 
+  let tabContent = <p> Please select a topic </p>
+  if (selectedTopic) {
+    tabContent = (
+      <div id='tab-content'>
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+    )
+  } 
+
   return (
     <div>
-      <Header/>
+      <Header/> 
       <main>
         <section id='core-concepts'>
           <h2>Core Concepts</h2>
@@ -42,17 +57,8 @@ function App() {
           <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
           <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id='tab-content'>
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-                {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
-
         <h2>Time to get started!</h2>
       </main>
     </div>
