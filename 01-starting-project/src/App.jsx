68c1abe2b511709c715c5e44c87b6dcbae8e4375
Moch. Luthfi Hadiwidjaja <Luthfihadi78@gmail.com>
2026-05-20 10:49:18 +0700

    add Examples to web

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -5,9 +5,10 @@ import Header from './components/Header/Header.jsx';
 import CoreConcepts from './components/CoreConcept';
 import TabButton from './components/TabButton.jsx';
 import { useState } from 'react';
+import { EXAMPLES } from './data.js'
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Please click a Button')
+  const [selectedTopic, setSelectedTopic] = useState('components')
 
   function handleSelect(selectedButton){
   setSelectedTopic(selectedButton)
@@ -41,7 +42,15 @@ function App() {
           <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
           <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id='tab-content'>
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
 
         <h2>Time to get started!</h2>
